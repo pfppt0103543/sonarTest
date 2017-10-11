8adba5944a2781ba4a5b08da65d85ce28f171b32
pfppt0103543 <pedro.pereira.t0103543@edu.atec.pt>
2017-10-11 15:57:09 +0100

    Update App.java

--- src/main/java/sonar_project/test/App.java
+++ src/main/java/sonar_project/test/App.java
@@ -8,15 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-       logger.log( "Hello World!" );
-       logger.log( "Hello World!" );
-        logger.log( "Hello World!" );
+
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-         logger.log( "HOPE, WE NEED HOPE!" );
-         logger.log( "HOPE, WE NEED HOPE!" );
          System.out.println( "HOPE, WE NEED HOPE!" );
          System.out.println( "HOPE, WE NEED HOPE!" );
          System.out.println( "HOPE, WE NEED HOPE!" );
